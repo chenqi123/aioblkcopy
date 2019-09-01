e6f744ea1b4a08d33ada872209990ca82630732d
Nikita Staroverov <nstaroverov@rtlservice.com>
2019-09-01 22:22:18 +0300

    Comleted move to cmake

--- src/aioblkcopy.c
+++ src/aioblkcopy.c
@@ -26,10 +26,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-//#define AIOBLKCOPY_DEBUG
-
-#include "config.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
